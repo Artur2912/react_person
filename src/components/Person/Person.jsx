f0c843e1ec0d3ae022aaeb096858c82ede3881e2
art <art@debian.art>
2025-09-30 20:39:09 +0300

    newsol

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,11 @@
 export const Person = ({ person }) => {
   const { name, age, partnerName, isMarried, sex } = person;
-
   const partner = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age && <p className="Person__age">I am {age}</p>}
-
+      {typeof age !== 'undefined' && <p className="Person__age">I am {age}</p>}
       {isMarried ? (
         <p className="Person__partner">
           {partnerName} is my {partner}
